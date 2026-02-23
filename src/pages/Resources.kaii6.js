1b8619724e857173ea4e02e546fb8a5fd6d3dd2a
lucas <lucas.martin.connelly@gmail.com>
2026-02-23 13:17:13 -0500

    Implemented Resource Fetching Logic

--- src/pages/Resources.kaii6.js
+++ src/pages/Resources.kaii6.js
@@ -1,15 +1,43 @@
-// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import { getPublicResources } from 'backend/dataService.jsw'
 
-$w.onReady(function () {
+$w.onReady(async function () { // On page load
+	$w("#noDataMessage").collapse();
+	$w("#resourceRepeater").collapse();
 
-	// Write your Javascript code here using the Velo framework API
+	try {
+		const allResources = await getPublicResources();
 
-	// Print hello world:
-	// console.log("Hello world!");
-
-	// Call functions on page elements, e.g.:
-	// $w("#button1").label = "Click me!";
-
-	// Click "Run", or Preview your site, to execute your code
-
+		if (allResources && allResources.length > 0) { // Data is present, populate resource repeater
+			$w("#resourceRepeater").expand();
+			setupRepeater(allResources);
+		}
+		else { // No data found, display #noDataMessage
+			$w("#noDataMessage").expand();
+		}
+	}
+	catch (error) {
+		console.error("Data fetch failed:", error);
+		$w("#noDataMessage").text = "System error. Please notify the admin.";
+		$w("#noDataMessage").expand();
+	}
 });
+
+function setupRepeater(data) {
+	$w("#resourceRepeater").onItemReady(($item, itemData) => {
+		$item("#titleText").text = itemData.title; // Display resource title
+		$item("#descriptionText").text = itemData.description; // Display resource description
+		if (itemData.file) { // Resource download button functionality
+			$item("#downloadButton").label = "Download PDF";
+			$item("#downloadButton").link = itemData.file;
+		}
+		else if (itemData.link) {
+			$item("#downloadButton").label = "Open Link";
+			$item("#downloadButton").link = itemData.link;
+		} else {
+			$item("downloadButton").hide();
+		}
+	});
+
+	$w("#resourceRepeater").data = data;
+}
+
