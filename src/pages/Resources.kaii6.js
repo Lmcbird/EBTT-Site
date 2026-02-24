029488e72393affa17333bd444d251c41a5dc16e
lucas <lucas.martin.connelly@gmail.com>
2026-02-24 13:19:20 -0500

    Implemented VIEW PDF button

--- src/pages/Resources.kaii6.js
+++ src/pages/Resources.kaii6.js
@@ -25,16 +25,26 @@ $w.onReady(async function () { // On page load
 function setupRepeater(data) {
 	$w("#resourceRepeater").onItemReady(($item, itemData) => {
 		$item("#titleText").text = itemData.title; // Display resource title
-		$item("#descriptionText").text = itemData.description; // Display resource description
-		if (itemData.file) { // Resource download button functionality
+		$item("#descriptionText").html = itemData.description; // Display resource description
+		if (itemData.file) { // Data is a file
+			// Resource view button functionality
+			$item("#button2").label = "View PDF";
+            $item("#button2").link = itemData.file;
+            $item("#button2").target = "_blank"; // Open in a new tab
+            $item("#button2").show();
+			// Resource download button functionality
 			$item("#downloadButton").label = "Download PDF";
-			$item("#downloadButton").link = itemData.file;
+			$item("#downloadButton").link = itemData.file + "?download=true";
+			$item("#downloadButton").show();
 		}
-		else if (itemData.link) {
+		else if (itemData.link) { // Data is a link to external site
+			$item("#button2").hide()
 			$item("#downloadButton").label = "Open Link";
 			$item("#downloadButton").link = itemData.link;
+			$item("#downloadButton").show();
 		} else {
-			$item("downloadButton").hide();
+			$item("#button2").hide();
+			$item("#downloadButton").hide();
 		}
 	});
 
