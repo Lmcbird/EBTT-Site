72a42fb1c8f8c76e197dbba4dbf49567e385717b
lucas <lucas.martin.connelly@gmail.com>
2026-03-05 16:42:34 -0500

    Implemented Partners Fetching Logic

--- src/pages/Partners.nj8dm.js
+++ src/pages/Partners.nj8dm.js
@@ -1,15 +1,43 @@
-// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import wixData from 'wix-data';
 
-$w.onReady(function () {
+$w.onReady(async function () {
+	$w("#noPartnersMessage").collapse();
+	$w("#partnersRepeater").collapse();
+    try {
+        const results = await wixData.query("Partners")
+            .eq("displayOnSite", true)
+            .find();
 
-	// Write your Javascript code here using the Velo framework API
-
-	// Print hello world:
-	// console.log("Hello world!");
-
-	// Call functions on page elements, e.g.:
-	// $w("#button1").label = "Click me!";
-
-	// Click "Run", or Preview your site, to execute your code
-
+        if (results.items.length > 0) {
+            setupPartnersRepeater(results.items);
+        } else { // Hide if empty
+            $w("#partnersRepeater").collapse();
+        }
+    } catch (err) {
+        console.error("Partners fetch failed", err);
+    }
 });
+
+function setupPartnersRepeater(data) {
+    $w("#partnersRepeater").onItemReady(($item, itemData) => {
+        $item("#partnerName").text = itemData.title; // Display Partner name 
+        
+        if (itemData.logo) { // Load Partner logo
+            $item("#partnerLogo").src = itemData.logo;
+            $item("#partnerLogo").tooltip = itemData.title;
+        }
+
+        if (itemData.website) { // Link the logo and a button to the partner's site 
+            $item("#partnerLogo").link = itemData.website;
+            $item("#partnerLogo").target = "_blank";
+            
+            $item("#partnerWebsite").link = itemData.website;
+            $item("#partnerWebsite").target = "_blank";
+            $item("#partnerWebsite").show();
+        } else {
+            $item("#partnerWebsite").hide();
+        }	
+    });
+
+	$w("#partnersRepeater").data = data;
+}
