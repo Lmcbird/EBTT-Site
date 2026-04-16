0570770ca4807e132981edda9c9256956acd6cd6
lucas <lucas.martin.connelly@gmail.com>
2026-04-16 17:46:18 -0400

    Added console logs for debugging

--- src/pages/About.juz8k.js
+++ src/pages/About.juz8k.js
@@ -1,26 +1,28 @@
 import wixData from 'wix-data';
 
 $w.onReady(async function () {
-	$w("#memberRepeater").collapse();
-	try {
-		const results = await wixData.query("Bios")
-					.eq("displayOnSite", true)
-					.ascending("name")
-					.find();
-		if (results.items.length > 0) {
+    $w("#memberRepeater").collapse();
+    try {
+        const results = await wixData.query("Bios")
+                    .eq("displayOnSite", true)
+                    .ascending("name")
+                    .find();
+        console.log("Query results:", results.items.length, "| First item:", JSON.stringify(results.items[0]));
+        if (results.items.length > 0) {
             setupMemberRepeater(results.items);
             $w("#memberRepeater").expand();
         }
-	} catch (err) {
+    } catch (err) {
         console.error("Fetch failed", err);
     }
 });
 
 function setupMemberRepeater(data) {
     $w("#memberRepeater").onItemReady(($item, itemData) => {
-		if (itemData.name) {
-			$item("#memberName").text = itemData.name || "";
-		}
+        console.log("Item ready:", itemData._id, itemData.name);
+        if (itemData.name) {
+            $item("#memberName").text = itemData.name || "";
+        }
         if (itemData.bio) {
             $item("#memberBio").text = itemData.bio;
             $item("#memberBio").expand();
@@ -28,10 +30,9 @@ function setupMemberRepeater(data) {
             $item("#memberBio").collapse();
         }
         if (itemData.image) {
-			$item("#memberPhoto").src = itemData.image;
+            $item("#memberPhoto").src = itemData.image;
             $item("#memberPhoto").tooltip = itemData.name;
         }
     });
-
     $w("#memberRepeater").data = data;
 }
