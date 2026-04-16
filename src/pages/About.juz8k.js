ddc9747d0872a230af6de0a4ecdd78401a1bd8e0
lucas <lucas.martin.connelly@gmail.com>
2026-04-16 14:11:06 -0400

    Implement member bios to about page

--- src/pages/About.juz8k.js
+++ src/pages/About.juz8k.js
@@ -1,15 +1,37 @@
-// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import wixData from 'wix-data';
 
-$w.onReady(function () {
-
-	// Write your Javascript code here using the Velo framework API
-
-	// Print hello world:
-	// console.log("Hello world!");
-
-	// Call functions on page elements, e.g.:
-	// $w("#button1").label = "Click me!";
-
-	// Click "Run", or Preview your site, to execute your code
-
+$w.onReady(async function () {
+	$w("#memberRepeater").collapse();
+	try {
+		const results = await wixData.query("Partners")
+					.eq("displayOnSite", true)
+					.ascending("name")
+					.find();
+		if (results.items.length > 0) {
+            setupMemberRepeater(results.items);
+            $w("#partnersRepeater").expand();
+        }
+	} catch (err) {
+        console.error("Fetch failed", err);
+    }
 });
+
+function setupMemberRepeater(data) {
+    $w("#memberRepeater").onItemReady(($item, itemData) => {
+		if (itemData.name) {
+			$item("#memberName").text = itemData.name || "";
+		}
+        if (itemData.bio) {
+            $item("#memberBio").text = itemData.bio;
+            $item("#memberBio").expand();
+        } else {
+            $item("#memberBio").collapse();
+        }
+        if (itemData.logo) {
+			$item("#memberPhoto").src = itemData.image;
+            $item("#memberPhoto").tooltip = itemData.name;
+        }
+    });
+
+    $w("#memberRepeater").data = data;
+}
